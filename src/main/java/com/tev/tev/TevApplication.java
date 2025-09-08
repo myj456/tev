0a28ddb83dff86c211d1777d2a0542237c6532ce
myj456 <bkspringmyj@gmail.com>
2025-09-08 10:33:43 +0900

    tev main server

---
+++ src/main/java/com/tev/tev/TevApplication.java
@@ -0,0 +1,13 @@
+package com.tev.tev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tev/tev/TevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tev.tev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
