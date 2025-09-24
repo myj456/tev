66f5561940a03ca6b31180b532dd89878b13816f
myj456 <bkspringmyj@gmail.com>
2025-09-24 10:47:52 +0900

    [feat]#3 - Api 응답 일관화 # 📝 본문 (Commit Body) - ApiResponse 추가
    # 기타 참고 사항
    - 관련 이슈: #3

---
+++ src/main/java/com/tev/tev/common/ApiResponse.java
@@ -0,0 +1,38 @@
+package com.tev.tev.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse<T> {
+    private boolean success;
+    private String message;
+    private List<String> errors;
+    private T data;
+
+    // 요청 성공 응답 - 데이터
+    public static <T> ApiResponse<T> success(T data){
+        return new ApiResponse<>(true, "요청 처리 성공", null, data);
+    }
+
+    // 요청 성공 응답 - 메세지 + 데이터
+    public static <T> ApiResponse<T> success(String message, T data){
+        return new ApiResponse<>(true, message, null, data);
+    }
+
+    // 요청 에러 - 메세지 + 에러메세지
+    public static <T> ApiResponse<T> error(String message, List<String> errors){
+        return new ApiResponse<>(false, message, errors, null);
+    }
+
+    // 요청 실패
+    public static <T> ApiResponse<T> fail(String message){
+        return new ApiResponse<>(false, message, null, null);
+    }
+
+}
